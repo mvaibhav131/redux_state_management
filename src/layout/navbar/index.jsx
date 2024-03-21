df75fc2d52fdd509153288233550b5ff0ea5ccd6
Vaibhav More <98808183+mvaibhav131@users.noreply.github.com>
2024-03-21 12:54:30 +0530

    navbar fixed

---
+++ src/layout/navbar/index.jsx
@@ -0,0 +1,67 @@
+import React from 'react';
+import { Avatar, Breadcrumb, Layout, Menu } from 'antd';
+import { HomeOutlined, UserOutlined } from '@ant-design/icons';
+import MainContent from '../../content';
+import "./index.css";
+import { useSelector } from 'react-redux';
+const { Header, Content, Footer } = Layout;
+
+const items = new Array(1).fill(null).map((_, index) => ({
+  key:"avatar",
+  label:"Graphical View Dashboard",
+}));
+const Navbar = () => {
+  const theme= useSelector(state => state.theme.theme)
+
+  return (
+    <Layout>
+      <Header className='navbar_header'
+      >
+        <div className="demo-logo" ><Avatar/></div>
+        <Menu
+          theme="dark"
+          mode="horizontal"
+          defaultSelectedKeys={'1'}
+          items={items}
+          style={{
+            flex: 1,
+            minWidth: 0,
+          }}
+        />
+       <Breadcrumb style={{background:"gray",padding:"6px",borderRadius:"11px"}}
+    items={[
+      {
+        href: '',
+        title: <HomeOutlined />,
+      },
+      {
+        href: '',
+        title: (
+          <>
+            <UserOutlined />
+            <span>Dashboard</span>
+          </>
+        ),
+      },
+      {
+        title: `${theme ==="dark"? "Dark Theme":"Light Theme"}`,
+      },
+    ]}
+  />
+      </Header>
+      <Content className='content'
+      >
+       
+        <div className='main_content'
+        >
+          <MainContent/>
+        </div>
+      </Content>
+      <Footer className='footers'
+      >
+        vaibhavmore.in ©{new Date().getFullYear()} Created by Vaibhav More
+      </Footer>
+    </Layout>
+  );
+};
+export default Navbar;
