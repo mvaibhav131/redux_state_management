fff7a815c3dfb0c97485a13d111e0f77c92a6a25
Vaibhav More <98808183+mvaibhav131@users.noreply.github.com>
2024-06-27 21:01:26 +0530

    Update index.jsx

--- src/store/index.jsx
+++ src/store/index.jsx
@@ -1,7 +1,7 @@
 
 import { configureStore } from "@reduxjs/toolkit";
 import themeReducer from "./Reducer/themeReducer";
-
+// store creation 
 const store = configureStore({
     reducer:{
         theme:themeReducer,
